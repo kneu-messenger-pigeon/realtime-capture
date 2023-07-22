b3012168ea4a65e19dee630b2cfcfe266f04f7c2
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-22 22:33:33 +0300

    Add script public url into variables

--- build-public.js
+++ build-public.js
@@ -5,11 +5,13 @@ const UglifyJS = require("uglify-js");
 
 require('toml-require').install();
 const wranglerConfig = require('./wrangler.toml')
+const { dirname } = require("path");
 
 const hostnamePlaceholder = '{WORKER_HOST}'
 
 const sourceFolder = 'src/';
 const captureJs = 'capture.js';
+const publicRoot = 'public/'
 const captureJsSource = UglifyJS.minify(
   fs.readFileSync(sourceFolder + captureJs, 'utf8')
 ).code
@@ -18,16 +20,28 @@ if (!captureJsSource.includes(hostnamePlaceholder)) {
   throw new Error('Capture.js doesn\'t include placeholder: ' + hostnamePlaceholder);
 }
 
+const publicUrlStorageDir = 'public-urls/'
+fs.mkdirSync(publicUrlStorageDir, { recursive: true })
+
 for (const envName in wranglerConfig.env) {
-  buildPublic(
-    envName,
-    wranglerConfig.env[envName].route.pattern
-  )
+  const captureJsPublicUrls = buildPublic(envName)
+
+  fs.writeFileSync(publicUrlStorageDir + envName, captureJsPublicUrls)
 }
 
-function buildPublic(env, hostname) {
-  const folder = 'public/' + env + '/';
-  const captureJsBuild = captureJsSource.replaceAll(hostnamePlaceholder, hostname)
-  fs.mkdirSync(folder, { recursive: true })
-  fs.writeFileSync(folder + captureJs, captureJsBuild)
+function buildPublic(env) {
+  const workerHostname = wranglerConfig.env[env].route.pattern
+  const staticHostname = wranglerConfig.env[env].route.zone_name
+
+
+  const captureJsPublicPathname = env + '/' + captureJs;
+  const captureJsFilepath = publicRoot + captureJsPublicPathname;
+
+  const captureJsBuild = captureJsSource.replaceAll(hostnamePlaceholder, workerHostname)
+  fs.mkdirSync(dirname(captureJsFilepath), { recursive: true })
+  fs.writeFileSync(captureJsFilepath, captureJsBuild)
+
+  const captureScriptUrl = new URL(captureJsPublicPathname, 'https://' + staticHostname)
+
+  return captureScriptUrl.href;
 }
--- get-environments-vars.sh
+++ get-environments-vars.sh
@@ -11,6 +11,14 @@ set | grep -v AWS_REGION | sort > ./.tmp.start.env-vars
 
 ### ------------------------------------------------------------------------------ ###
 
+SCRIPT_PUBLIC_URL=$(cat public-urls/${ENV})
+
+[ $ENV != "prod" ] && SCRIPT_PROD_PUBLIC_URL=$(cat public-urls/prod)
+
+>&2 echo "done SCRIPT_PUBLIC_URL: ${SCRIPT_PUBLIC_URL}"
+### ------------------------------------------------------------------------------ ###
+
+
 
 AWS_REGION=$( aws configure list | grep region | awk '{print $2}')
 >&2 echo "done AWS_REGION: ${AWS_REGION}"
@@ -35,8 +43,8 @@ PRODUCER_AWS_ACCESS_KEY_ID=$(
 PRODUCER_AWS_SECRET_ACCESS_KEY=$(
   aws ssm get-parameter --name /realtime-changes-${ENV}-producer-user/secret --output text --query Parameter.Value
 )
-
->&2 echo "done PRODUCER_AWS_SECRET_ACCESS_KEY..."
+>&2 echo "::add-mask::$PRODUCER_AWS_SECRET_ACCESS_KEY"
+>&2 echo "done PRODUCER_AWS_SECRET_ACCESS_KEY: $PRODUCER_AWS_SECRET_ACCESS_KEY..."
 ### ------------------------------------------------------------------------------ ###
 
 
@@ -53,23 +61,8 @@ CONSUMER_AWS_ACCESS_KEY_ID=$(
 CONSUMER_AWS_SECRET_ACCESS_KEY=$(
    aws ssm get-parameter --name /realtime-changes-${ENV}-consumer-user/secret --output text --query Parameter.Value
 )
->&2 echo "done CONSUMER_AWS_SECRET_ACCESS_KEY:..."
-### ------------------------------------------------------------------------------ ###
-
-
-
-
-SCRIPT_PUBLIC_PATH=$(cd src && ls *.js | head -n 1;  cd ..);
-SCRIPT_PUBLIC_URL=$(
-  node<<EOF
-      require('toml-require').install();
-      const wranglerConfig = require('./wrangler.toml');
-      console.log("https://" + wranglerConfig.env['${ENV}'].route['zone_name'] + "/${ENV}/${SCRIPT_PUBLIC_PATH}");
-EOF
-)
-
-unset SCRIPT_PUBLIC_PATH
->&2 echo "done SCRIPT_PUBLIC_URL: ${SCRIPT_PUBLIC_URL}"
+>&2 echo "::add-mask::$CONSUMER_AWS_SECRET_ACCESS_KEY"
+>&2 echo "done CONSUMER_AWS_SECRET_ACCESS_KEY: $CONSUMER_AWS_SECRET_ACCESS_KEY..."
 ### ------------------------------------------------------------------------------ ###
 
 
